cb6fd1f9310eb02cc4e006873ad53b1f86bdf938
Mauricio Fernandes <mauriciodiasf@gmail.com>
2016-03-09 23:48:19 -0300

    added ng-repeat

--- public/js/controllers/ContatosController.js
+++ public/js/controllers/ContatosController.js
@@ -1,7 +1,24 @@
 angular.module('contatooh').controller('ContatosController',
   function($scope) {
-     $scope.total = 0;
-     $scope.incrementa = function() {
-      $scope.total++;
-    };
+  $scope.total = 0;
+  $scope.incrementa = function() {
+    $scope.total++;
+  };
+  $scope.contatos = [
+    {
+      "_id" : 1,
+      "nome" : "Contato angular 1",
+      "email" : "contato@empresa.com.br"
+    },
+    {
+      "_id" : 2,
+      "nome" : "Contato angular 2",
+      "email" : "contato2@empresa.com.br"
+    },
+    {
+      "_id" : 3,
+      "nome" : "Contato angular 3",
+      "email" : "contato3@empresa.com.br"
+    }
+  ];
 });
