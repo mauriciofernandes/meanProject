1d7bc1174f13f48488452e95b40405fbd084b0b6
Mauricio Fernandes <mfernand@motorola.com>
2016-03-11 17:44:17 -0300

    created the first service

--- public/js/controllers/ContatosController.js
+++ public/js/controllers/ContatosController.js
@@ -1,10 +1,9 @@
 angular.module('contatooh').controller('ContatosController',
-  function($scope, $resource) {
+  function($scope, Contato) {
     $scope.contatos = [];
     $scope.filtro = '';
     $scope.mensagem = {texto: ''};
-    var Contato = $resource('/contatos/:id');
-
+    
     function buscaContatos() {
       Contato.query(
         function(contatos){
